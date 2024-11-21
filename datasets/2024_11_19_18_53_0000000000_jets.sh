970fe7ec70aba665ae12222bdbdb5b1a417fa3f0
lucasbanunes <lucasbanunes@gmail.com>
2024-11-20 21:00:50 -0300

    Adds more datasets

--- datasets/2024_11_19_18_53_0000000000_jets.sh
+++ datasets/2024_11_19_18_53_0000000000_jets.sh
@@ -8,27 +8,27 @@ NOV=$1 && \
 n_workers=$2 && \
 seed=2137420 && \
 base_dir="${LZT_DATA}/2024_11_19_18_53_0000000000_jets"
-if [ -d "$base_dir" ]; then
-    echo "Directory $base_dir already exists. Exiting..."
-    exit 1
-fi
+# if [ -d "$base_dir" ]; then
+#     echo "Directory $base_dir already exists. Exiting..."
+#     exit 1
+# fi
 lzt_path="${HOME}/workspaces/lorenzetti/lorenzetti" && \
 cd $lzt_path && source setup_envs.sh && source setup.sh && \
 # generate events with pythia
-mkdir -p "${base_dir}/EVT" && cd "${base_dir}/EVT" && \
-echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Started EVT sim" > "${base_dir}/started_EVT.log" && \
-(prun_evts.py -c "gen_jets.py --pileupAvg 0 --nov %NOV --eventNumber %OFFSET -o %OUT -s %SEED" -nt $n_workers --nov $NOV --seed $seed --novPerJob 200 -o "${base_dir}/EVT/jf17.EVT.root" |& tee "${base_dir}/jf17.EVT.log")  && \
-echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Finished EVT sim" > "${base_dir}/finished_EVT.log" && \
-# generate hits around the truth particle seed
-mkdir -p "${base_dir}/HIT" && cd "${base_dir}/HIT" && \
-echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Started HIT sim" > "${base_dir}/started_HIT.log" && \
-(prun_jobs.py -c "simu_trf.py -i %IN -o %OUT -nt $n_workers -t 10" -nt 1 -i "${base_dir}/EVT" -o "${base_dir}/HIT/jf17.HIT.root" |& tee "${base_dir}/jf17.HIT.log") && \
-echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Finished HIT sim" > "${base_dir}/finished_HIT.log" && \
-# digitalization
-mkdir -p "${base_dir}/ESD" && cd "${base_dir}/ESD" && \
-echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Started ESD sim" > "${base_dir}/started_ESD.log" && \
-(prun_jobs.py -c "digit_trf.py -i %IN -o %OUT" -i "${base_dir}/HIT" -o "${base_dir}/ESD/jf17.ESD.root" -nt $n_workers |& tee "${base_dir}/jf17.ESD.log") && \
-echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Finished ESD sim" > "${base_dir}/finished_ESD.log" && \
+# mkdir -p "${base_dir}/EVT" && cd "${base_dir}/EVT" && \
+# echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Started EVT sim" > "${base_dir}/started_EVT.log" && \
+# (prun_evts.py -c "gen_jets.py --pileupAvg 0 --nov %NOV --eventNumber %OFFSET -o %OUT -s %SEED" -nt $n_workers --nov $NOV --seed $seed --novPerJob 200 -o "${base_dir}/EVT/jf17.EVT.root" |& tee "${base_dir}/jf17.EVT.log")  && \
+# echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Finished EVT sim" > "${base_dir}/finished_EVT.log" && \
+# # generate hits around the truth particle seed
+# mkdir -p "${base_dir}/HIT" && cd "${base_dir}/HIT" && \
+# echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Started HIT sim" > "${base_dir}/started_HIT.log" && \
+# (prun_jobs.py -c "simu_trf.py -i %IN -o %OUT -nt $n_workers -t 10" -nt 1 -i "${base_dir}/EVT" -o "${base_dir}/HIT/jf17.HIT.root" |& tee "${base_dir}/jf17.HIT.log") && \
+# echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Finished HIT sim" > "${base_dir}/finished_HIT.log" && \
+# # digitalization
+# mkdir -p "${base_dir}/ESD" && cd "${base_dir}/ESD" && \
+# echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Started ESD sim" > "${base_dir}/started_ESD.log" && \
+# (prun_jobs.py -c "digit_trf.py -i %IN -o %OUT" -i "${base_dir}/HIT" -o "${base_dir}/ESD/jf17.ESD.root" -nt $n_workers |& tee "${base_dir}/jf17.ESD.log") && \
+# echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Finished ESD sim" > "${base_dir}/finished_ESD.log" && \
 # reconstruction
 mkdir -p "${base_dir}/AOD" && cd "${base_dir}/AOD" && \
 echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Started AOD sim" > "${base_dir}/started_AOD.log" && \
--- datasets/README.md
+++ datasets/README.md
@@ -13,17 +13,33 @@ The lorenzetti source code can be seen [here](https://github.com/lorenzetti-hep/
 - Commit: [333841f7e8254dc3454779fea0882d61720f9458](https://github.com/lorenzetti-hep/lorenzetti/commit/333841f7e8254dc3454779fea0882d61720f9458)
 - Seed: 242746
 
-## 2024_08_19_21_18_0000000000_single_electron
-- 10k samples
-- Commit: [333841f7e8254dc3454779fea0882d61720f9458](https://github.com/lorenzetti-hep/lorenzetti/commit/333841f7e8254dc3454779fea0882d61720f9458)
-- Seed: 108625
-
 ## 2024_08_19_21_08_0000000000_zee
 - 10k samples
 - Commit: [333841f7e8254dc3454779fea0882d61720f9458](https://github.com/lorenzetti-hep/lorenzetti/commit/333841f7e8254dc3454779fea0882d61720f9458)
 - Seed: 87483
 
+## 2024_08_19_21_18_0000000000_single_electron
+- 10k samples
+- Commit: [333841f7e8254dc3454779fea0882d61720f9458](https://github.com/lorenzetti-hep/lorenzetti/commit/333841f7e8254dc3454779fea0882d61720f9458)
+- Seed: 108625
+
 ## 2024_08_20_21_08_0000000000_zee
 - 100k samples
 - Commit: [333841f7e8254dc3454779fea0882d61720f9458](https://github.com/lorenzetti-hep/lorenzetti/commit/333841f7e8254dc3454779fea0882d61720f9458)
 - Seed: 13443634
+
+## 2024_08_22_12_00_0000000000_minibias
+- Aimed for 10k samples generated only 1968
+The time for HIT execution on geant was too long and always gave timeout. The EVT files worked fine.
+- Commit: [333841f7e8254dc3454779fea0882d61720f9458](https://github.com/lorenzetti-hep/lorenzetti/commit/333841f7e8254dc3454779fea0882d61720f9458)
+- Seed: 34017
+
+## 2024_09_17_08_00_0000000000_minibias
+- Aimed for 10k samples generated only 1968
+The time for HIT execution on geant was too long and always gave timeout. The EVT files worked fine.
+- Commit: [333841f7e8254dc3454779fea0882d61720f9458](https://github.com/lorenzetti-hep/lorenzetti/commit/333841f7e8254dc3454779fea0882d61720f9458)
+- Seed: 34017
+- Had a different pythia config from the previous minibias
+
+## 2024_11_19_18_53_0000000000_jets
+- WIP
