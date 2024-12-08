b2dfa71af13974dd5cfab80eb76bde8d3c470f1f
lucasbanunes <lucasbanunes@gmail.com>
2024-12-08 20:37:29 -0300

    Debug changes

--- datasets/2024_11_19_18_53_0000000000_jets.sh
+++ datasets/2024_11_19_18_53_0000000000_jets.sh
@@ -1,11 +1,11 @@
 # Generates a dataset out of jets
 # Usage example 
-# 2024_11_19_18_53_0000000000_jets.sh <n_events> <n_workers>
-# 2024_11_19_18_53_0000000000_jets.sh 100 10
-# This will generate a dataset with 100 events using 10 workers
+# 2024_11_19_18_53_0000000000_jets.sh <n_workers>
+# 2024_11_19_18_53_0000000000_jets.sh 10
+# This will generate a dataset using 10 workers
 call_dir=$PWD
-NOV=$1 && \
-n_workers=$2 && \
+NOV=10000 && \
+n_workers=$1 && \
 seed=2137420 && \
 base_dir="${LZT_DATA}/2024_11_19_18_53_0000000000_jets"
 # if [ -d "$base_dir" ]; then
@@ -32,12 +32,12 @@ cd $lzt_path && source setup_envs.sh && source setup.sh && \
 # reconstruction
 mkdir -p "${base_dir}/AOD" && cd "${base_dir}/AOD" && \
 echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Started AOD sim" > "${base_dir}/started_AOD.log" && \
-(prun_jobs.py -c "reco_trf.py -i %IN -o %OUT" -i "${base_dir}/ESD" -o "${base_dir}/AOD/jf17.AOD.root" -nt $n_workers -m |& tee "${base_dir}/jf17.AOD.log" )  && \
-echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Finished AOD sim" > "${base_dir}/finished_AOD.log" && \
+(prun_jobs.py -c "reco_trf.py -i %IN -o %OUT -l DEBUG" -i "${base_dir}/ESD" -o "${base_dir}/AOD/jf17.AOD.root" -nt $n_workers |& tee "${base_dir}/jf17.AOD.log" )  && \
+echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Finished AOD sim" > "${base_dir}/finished_AOD.log"
 # ntuple
-mkdir -p "${base_dir}/NTUPLE" && cd "${base_dir}/NTUPLE" && \
-echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Started NTUPLE sim" > "${base_dir}/started_NTUPLE.log" && \
-(ntuple_trf.py -i "${base_dir}/AOD/jf17.AOD.root" -o "${base_dir}/NTUPLE/jf17.NTUPLE.root" |& tee "${base_dir}/jf17.NTUPLE.log" )  && \
-echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Finished NTUPLE sim" > "${base_dir}/finished_NTUPLE.log"
+# mkdir -p "${base_dir}/NTUPLE" && cd "${base_dir}/NTUPLE" && \
+# echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Started NTUPLE sim" > "${base_dir}/started_NTUPLE.log" && \
+# (ntuple_trf.py -i "${base_dir}/AOD/jf17.AOD.root" -o "${base_dir}/NTUPLE/jf17.NTUPLE.root" |& tee "${base_dir}/jf17.NTUPLE.log" )  && \
+# echo "$(date -d "today" +"%Y/%m/%d %H-%M-%s") - Finished NTUPLE sim" > "${base_dir}/finished_NTUPLE.log"
 
 cd $call_dir
--- scripts/build_lzt.sh
+++ scripts/build_lzt.sh
@@ -9,5 +9,5 @@ build_dir=$lzt_repo/build
 if [ -d "$build_dir" ] && [ "$1" == "overwrite" ]; then
     rm -r $build_dir
 fi
-cd $lzt_repo && (make |& tee $log_path) && source setup.sh
+cd $lzt_repo && source setup_envs.sh && (make |& tee $log_path) && source setup.sh
 cd $call_dir
