235bcc85e1e37b8952b6c8e920756d74690e24eb
lucasbanunes <lucasbanunes@gmail.com>
2024-12-16 01:21:06 +0000

    Fixes scripts/fit_cnns.sh

--- scripts/fit_cnns.sh
+++ scripts/fit_cnns.sh
@@ -1,13 +1,14 @@
 N_FOLDS=5
-python fit_cnn.py \
-    --sig-dir "/root/ext_data/lorenzetti/zee-mb-2024-12-08" \
-    --bkg-dir "/root/ext_data/lorenzetti/jet-mb-2024-12-08" \
-    --output-dir "/root/ext_data/aprendizado-profundo/models-2024-12-15/cnnv1" \
-    --dense-layers 5 \
-    --n-folds $N_FOLDS \
-    --kernel-sizes 2 4 \
-    --incep-modules 1 \
-    --n-filters 2
+# python fit_cnn.py \
+#     --sig-dir "/root/ext_data/lorenzetti/zee-mb-2024-12-08" \
+#     --bkg-dir "/root/ext_data/lorenzetti/jet-mb-2024-12-08" \
+#     --output-dir "/root/ext_data/aprendizado-profundo/models-2024-12-15/cnnv1" \
+#     --dense-layers 5 \
+#     --n-folds $N_FOLDS \
+#     --kernel-sizes 2 4 \
+#     --incep-modules 1 \
+#     --n-filters 2
+
 python fit_cnn.py \
     --sig-dir "/root/ext_data/lorenzetti/zee-mb-2024-12-08" \
     --bkg-dir "/root/ext_data/lorenzetti/jet-mb-2024-12-08" \
@@ -15,18 +16,18 @@ python fit_cnn.py \
     --dense-layers 5 \
     --n-folds $N_FOLDS \
     --kernel-sizes 2 4 \
-    --incep-modules 1 2 \
+    --incep-modules 2 \
     --n-filters 2
 
-python fit_cnn.py \
-    --sig-dir "/root/ext_data/lorenzetti/zee-mb-2024-12-08" \
-    --bkg-dir "/root/ext_data/lorenzetti/jet-mb-2024-12-08" \
-    --output-dir "/root/ext_data/aprendizado-profundo/models-2024-12-15/cnnv3" \
-    --dense-layers 5 \
-    --n-folds $N_FOLDS \
-    --kernel-sizes 2 4 \
-    --incep-modules 1 \
-    --n-filters 4
+# python fit_cnn.py \
+#     --sig-dir "/root/ext_data/lorenzetti/zee-mb-2024-12-08" \
+#     --bkg-dir "/root/ext_data/lorenzetti/jet-mb-2024-12-08" \
+#     --output-dir "/root/ext_data/aprendizado-profundo/models-2024-12-15/cnnv3" \
+#     --dense-layers 5 \
+#     --n-folds $N_FOLDS \
+#     --kernel-sizes 2 4 \
+#     --incep-modules 1 \
+#     --n-filters 4
 
 python fit_cnn.py \
     --sig-dir "/root/ext_data/lorenzetti/zee-mb-2024-12-08" \
@@ -35,5 +36,5 @@ python fit_cnn.py \
     --dense-layers 5 \
     --n-folds $N_FOLDS \
     --kernel-sizes 2 4 \
-    --incep-modules 1 2 \
+    --incep-modules 2 \
     --n-filters 4
