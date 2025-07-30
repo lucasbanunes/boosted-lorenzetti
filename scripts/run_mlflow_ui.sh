e4a7d6c735a0f1b3cbdcd22d1bcb05bbe1d694fe
lucasbanunes <lucasbanunes@gmail.com>
2025-07-30 10:39:48 -0300

    Updates run_mlflow_ui.sh for compatibility with sluram's sbatch

--- scripts/run_mlflow_ui.sh
+++ scripts/run_mlflow_ui.sh
@@ -1,4 +1,22 @@
-lzt_dataset_path=$1
-port=$2
+#!/bin/bash
+#SBATCH --job-name=mlflow-lzt
+#SBATCH --partition=gpu
+#SBATCH --cpus-per-task=8
+#SBATCH -o /home/lucas.nunes/logs/boosted-lorenzetti/%x-%j.out
+# %x is the job name
+# %j is the job ID
 
-cd ${lzt_dataset_path} && conda run -n dev mlflow ui -h 0.0.0.0 -p ${port}
+# Launches dev environment
+# Usage
+# $ sbatch run_mlflow_ui.sh <img> <port>
+# $ sbatch run_mlflow_ui.sh boosted-lorenzetti_0.1.0.sif 5000
+
+img=$1
+port=$2
+command="cd /mnt/cern_data/${USER}/lorenzetti && conda run -n dev mlflow ui -h 0.0.0.0 -p ${port}"
+# "cd /mnt/cern_data/${USER}/lorenzetti && conda run -n dev mlflow ui -h 0.0.0.0 -p ${port}"
+
+echo "Running command ${command} on ${img}"
+singularity exec \
+    --bind /mnt/cern_data:/mnt/cern_data \
+    $SIF_IMGS_DIR/$img /usr/bin/bash -c "${command}"
