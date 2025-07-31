3a902caf50addedce63aa9722ef71ed64c512745
lucasbanunes <lucasbanunes@gmail.com>
2025-07-31 08:47:51 -0300

    Updates script run_mlp_training.sh

--- scripts/zee-jf17-250pileup/run_mlp_training.sh
+++ scripts/zee-jf17-250pileup/run_mlp_training.sh
@@ -7,15 +7,16 @@
 # %j is the job ID
 
 # Usage
-# $ sbatch run_mlp_training.sh <img> <run-id> <lzt_dataset_path>
+# $ sbatch run_mlp_training.sh <img> <lzt_dataset_path> <run-id-1., run-id-2, ...>
 
 img=$1
-run_id=$2
-lzt_dataset_path=$3
+lzt_dataset_path=$2
+run_ids=${@:3}
 
-command="cd /home/${USER}/workspaces/lorenzetti/boosted-lorenzetti && conda run -n dev python cli.py mlp run-training ${run_id} --tracking-uri file://${lzt_dataset_path}/mlruns --experiment-name boosted-lorenzetti"
+command="cd /home/${USER}/workspaces/lorenzetti/boosted-lorenzetti && conda run -n dev python cli.py mlp run-training ${run_ids} --tracking-uri file://${lzt_dataset_path}/mlruns --experiment-name boosted-lorenzetti"
 
 echo "Running command ${command} on ${img}"
 singularity exec \
+    --nv \
     --bind /mnt/cern_data:/mnt/cern_data \
     $SIF_IMGS_DIR/$img /usr/bin/bash -c "${command}"
